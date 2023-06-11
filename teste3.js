dac5048469d30f4a9225af3d6beda50a63ce576f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-06-11 19:01:42 -0300

    feat: implementando função para deletar um usuário pelo nome

--- teste3.js
+++ teste3.js
@@ -1,15 +1,29 @@
 var data =  require("./fakeData");
 
 module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+    var { name } =  req.query;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+    if (!name) {
+        return res.status(400).json({
+            message: 'Please provide name.'
+        });
     }
 
-    res.send("success");
+    const user = data.find(user => user.name === name);
 
+    if(!user) {
+        return res.status(404).json({
+            message: 'User not found.'
+        });
+    }
+
+    data.map((user, index) => {
+        if(user.name === name) {
+            data.splice(index, 1);
+        }
+    })
+
+    return res.status(200).send({
+        message: 'User deleted successfully.'
+    });
 };
